b12cd39da3a2b80d61dcf6e05d1102b6f1743d89
kimdev <kim__dev@hufs.ac.kr>
2024-11-13 11:08:02 +0900

    init: first commit

---
+++ src/main/java/com/ganzithon/Hexfarming/HexfarmingApplication.java
@@ -0,0 +1,13 @@
+package com.ganzithon.Hexfarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexfarmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexfarmingApplication.class, args);
+	}
+
+}
