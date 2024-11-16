138e8ba97d4b3cb7202836867be949d094fc43ac
kimdev <kim__dev@hufs.ac.kr>
2024-11-16 18:45:56 +0900

    refactor(Ability, Tier): enum에 고유 id 부여

--- src/main/java/com/ganzithon/Hexfarming/global/enumeration/Ability.java
+++ src/main/java/com/ganzithon/Hexfarming/global/enumeration/Ability.java
@@ -4,18 +4,20 @@ import lombok.Getter;
 
 @Getter
 public enum Ability {
-    LEADERSHIP("리더십", ""),
-    PROBLEM_SOLVING("", ""),
-    COMMUNICATION_SKILL("소통 역량", ""),
-    DILIGENCE("성실성", ""),
-    RESILIENCE("회복 탄력성", ""),
-    TENACITY("인성", "");
+    LEADERSHIP(0, "리더십", ""),
+    PROBLEM_SOLVING(1, "", ""),
+    COMMUNICATION_SKILL(2, "소통 역량", ""),
+    DILIGENCE(3, "성실성", ""),
+    RESILIENCE(4, "회복 탄력성", ""),
+    TENACITY(5, "인성", "");
 
 
+    private final int id;
     private final String name;
     private final String description;
 
-    Ability(String name, String description) {
+    Ability(int id, String name, String description) {
+        this.id = id;
         this.name = name;
         this.description = description;
     }
