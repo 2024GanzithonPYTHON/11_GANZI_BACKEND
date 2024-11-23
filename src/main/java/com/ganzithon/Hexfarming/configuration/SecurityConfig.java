74d0cd4de1e3f4b7d809bc37968d90be2b593ee4
kimdev <kim__dev@hufs.ac.kr>
2024-11-23 10:07:49 +0900

    fix(Security): CORS 수정

--- src/main/java/com/ganzithon/Hexfarming/configuration/SecurityConfig.java
+++ src/main/java/com/ganzithon/Hexfarming/configuration/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
+        configuration.addAllowedOrigin("http://localhost:3000");
         configuration.addAllowedOrigin("http://127.0.0.1:3000");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
